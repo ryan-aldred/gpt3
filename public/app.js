3a1be02cf5aae176810f8496b5d115ed328b4a6e
Ryan <admin@ryanaldred.ca>
2022-05-14 14:44:49 -0700

    refactor

--- public/app.js
+++ public/app.js
@@ -13,11 +13,10 @@ class ChatBot extends HTMLElement {
   handlePromptSubmit = (evt) => {
     evt.preventDefault();
     console.log(this);
+
     const formData = new FormData(this.querySelector('.chatbot__form'));
 
-
     let prompt = [...formData][0][1];
-
     const apiKey = secret_api_key;
     const chatLog = 'Human: Hello, who are you?\nAI: I am doing great. How can I help you today?\n';
     const question = 'Could you tell me what your favorite German thrash metal album is?';
@@ -30,8 +29,7 @@ class ChatBot extends HTMLElement {
       presence_penalty: 0.6,
       'stop': '\nHuman'
     };
-  
-  
+    
     fetch('https://api.openai.com/v1/engines/ada/completions', {
       method: 'POST',
       headers: {
@@ -45,17 +43,32 @@ class ChatBot extends HTMLElement {
       console.log(prompt)
       console.log(data);
 
-      const chat = document.createElement('li');
-      chat.className = 'chatbox__chat';
-      chat.dataset.created = data.created;
-      chat.innerHTML = `
-        <div>Prompt: ${prompt}</div>
-        <div>Response: ${data.choices[0].text}</div>
-      `;
-      this.chatbox.appendChild(chat);
+      const chatData = this.parseGptResponse(prompt, data);
+      this.renderChatItem(chatData);
     })
     .catch(error => console.log(error));
-  } 
+  }
+
+  parseGptResponse = (prompt, responseData) => {
+    return {
+      prompt: prompt,
+      response: responseData.choices[0].text,
+      id: responseData.created,
+    }
+  }
+
+  renderChatItem = ({prompt, response, id}) => {
+    console.log('renderChatItem', );
+
+    const chat = document.createElement('li');
+    chat.className = 'chatbox__chat';
+    chat.dataset.id = id;
+    chat.innerHTML = `
+      <div>Prompt: ${prompt}</div>
+      <div>Response: ${response}</div>
+    `;
+    this.chatbox.appendChild(chat);
+  }
 }
 
 customElements.define('chat-bot', ChatBot);
